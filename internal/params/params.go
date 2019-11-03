e46edaf81d3d3b1f82dd0cf994ad20b54e7c12b0
Quentin McGaw (desktop) <quentin.mcgaw@gmail.com>
2019-11-03 17:32:32 +0000

    Uses underscore in environment variables

--- internal/params/params.go
+++ internal/params/params.go
@@ -10,16 +10,16 @@ import (
 )
 
 // GetOutputDir obtains the output directory path to write files to
-// from the environment variable OUTPUTDIR and defaults to ./files
+// from the environment variable OUTPUT_DIR and defaults to ./files
 func GetOutputDir() (path string, err error) {
-	return libparams.GetPath("OUTPUTDIR", "./files")
+	return libparams.GetPath("OUTPUT_DIR", "./files")
 }
 
 // GetNamedRootMD5 obtains the MD5 Hex encoded checksum for the named root
-// from the environment variable NAMEDROOTMD5. It defaults to
+// from the environment variable NAMED_ROOT_MD5. It defaults to
 // 25659425b11bb58ece6306d0cfe4b587
 func GetNamedRootMD5() (namedRootMD5 string, err error) {
-	s := libparams.GetEnv("NAMEDROOTMD5", "23ec4e704cdaa1dcaaa6f66bc2c0563f")
+	s := libparams.GetEnv("NAMED_ROOT_MD5", "23ec4e704cdaa1dcaaa6f66bc2c0563f")
 	if !verification.MatchMD5String(s) {
 		return "", fmt.Errorf("%s is not a 32 hexadecimal MD5 string", s)
 	}
@@ -27,10 +27,10 @@ func GetNamedRootMD5() (namedRootMD5 string, err error) {
 }
 
 // GetRootAnchorsSHA256 obtains the SHA256 Hex encoded checksum for the root anchors
-// from the environment variable ROOTANCHORSSHA256. It defaults to
+// from the environment variable ROOT_ANCHORS_SHA256. It defaults to
 // 45336725f9126db810a59896ae93819de743c416262f79c4444042c92e520770
 func GetRootAnchorsSHA256() (rootAnchorsSHA256 string, err error) {
-	s := libparams.GetEnv("ROOTANCHORSSHA256", "45336725f9126db810a59896ae93819de743c416262f79c4444042c92e520770")
+	s := libparams.GetEnv("ROOT_ANCHORS_SHA256", "45336725f9126db810a59896ae93819de743c416262f79c4444042c92e520770")
 	if !verification.MatchSHA256String(s) {
 		return "", fmt.Errorf("%s is not a 64 hexadecimal SHA256 string", s)
 	}
@@ -48,14 +48,14 @@ func GetPeriodMinutes() (periodMinutes time.Duration, err error) {
 }
 
 // GetResolveHostnames obtains 'yes' or 'no' to resolve hostnames in order to obtain
-// more IP addresses. If you are blocking the hostname resolution on your network,
-// you might want this feature off.
+// more IP addresses, from the environment variable RESOLVE_HOSTNAMES.
+// If you are blocking the hostname resolution on your network, turn this feature off.
 func GetResolveHostnames() (resolveHostnames bool, err error) {
-	s := libparams.GetEnv("RESOLVEHOSTNAMES", "no")
+	s := libparams.GetEnv("RESOLVE_HOSTNAMES", "no")
 	if s == "yes" {
 		return true, nil
 	} else if s == "no" {
 		return false, nil
 	}
-	return false, fmt.Errorf("RESOLVEHOSTNAMES value %q can only be 'yes' or 'no'", s)
+	return false, fmt.Errorf("RESOLVE_HOSTNAMES value %q can only be 'yes' or 'no'", s)
 }
